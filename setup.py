e7f8b2f474db2d26a9d24b7cc6f0c337b27adf64
simonefenix <simone.murzilli@fao.org>
2015-02-05 14:56:42 +0100

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='GeobricksSpatialQuery',
-    version='0.0.4',
+    version='0.0.5',
     author='Simone Murzilli; Guido Barbaglia',
     author_email='geobrickspy@gmail.com',
     packages=find_packages(),
