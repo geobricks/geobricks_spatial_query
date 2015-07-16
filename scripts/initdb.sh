f12cf5a2381b236b5af8c492fc864360c5d84618
simonefenix <simone.murzilli@fao.org>
2015-07-16 11:49:01 +0200

    refactoring

--- scripts/initdb.sh
+++
@@ -1,8 +0,0 @@
-sudo -u postgres createuser --superuser spatialuser
-sudo -u postgres psql
-\password spatialuser
-#insert password (spatial)
-#\q
-createdb -U spatialuser spatialdbtest
-psql -U spatialuser -d spatialdbtest -c "CREATE EXTENSION postgis;"
-shp2pgsql -W LATIN1 -I -s 4326 ne_110m_admin_0_countries.shp spatial.ne_110m_admin_0_countries | psql -U spatialuser spatialdbtest
